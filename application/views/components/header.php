13566296e2e14091433ca3a0ae7d8bd5451ddbfc
haktan.akdag <haktan.akdag@hotmail.com>
2021-10-22 10:12:08 +0300

    .

--- application/views/components/header.php
+++ application/views/components/header.php
@@ -1,2 +1,28 @@
-<?php linkCSS("assets/css/bootstrap.min.css"); ?>
-<?php linkCSS("assets/css/style.css") ?>
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>VOID SYS Web </title>
+    <meta name="description" content="Sufee Admin - HTML5 Admin Template">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="apple-touch-icon" href="apple-icon.png">
+    <link rel="shortcut icon" href="favicon.ico">
+
+    <link rel="stylesheet" href="../public/vendors/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../public/vendors/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../public/vendors/themify-icons/css/themify-icons.css">
+    <link rel="stylesheet" href="../public/vendors/flag-icon-css/css/flag-icon.min.css">
+    <link rel="stylesheet" href="../public/vendors/selectFX/css/cs-skin-elastic.css">
+    <link rel="stylesheet" href="../public/vendors/jqvmap/dist/jqvmap.min.css">
+
+
+    <link rel="stylesheet" href="../public/assets/css/style.css">
+
+    <link href='https:/fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
+
+</head>
+
+<body>
